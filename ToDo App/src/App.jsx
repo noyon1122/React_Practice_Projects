d8bf4e18069345763a0ab4a094fa08b4a16f8958
noyon122 <noyonhossain910@gmail.com>
2024-11-06 17:56:40 +0600

    ToDo App done

--- ToDo App/src/App.jsx
+++ ToDo App/src/App.jsx
@@ -1,12 +1,16 @@
 
-import { Link } from 'react-router-dom'
+
 import './App.css'
 import { useState } from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+ 
 
   
   const backgroundToday= document.getElementById('today') ;
@@ -45,43 +49,90 @@ function App() {
     backgroundPending.style.color=''
   }
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [startDate,setStartDate]=useState(new Date());
+  const [tasks,setTasks]=useState([])
+
   //habdle Add task
-  const handleAddTasks =()=>{
-        
+
+  const handleAddTasks =(e)=>{
+        e.preventDefault();
+        const form=e.target;
+        const title=form.title.value;
+        const deadline=startDate;
+        const comments=form.comments.value;
+        const priority=form.priority.value;
+
+        const allTask={title,deadline,comments,priority}
+        setTasks([...tasks,allTask])
+      
+  }
+
+  //Delete a specific task
+
+  const handleDelete=index=>{
+    const newTask=[...tasks];
+    console.log(newTask)
+    newTask.splice(index,1);
+    setTasks(newTask);
+  }
+
+  //Edit a spcific task
+
+  const handleEdit =index=>{
+
   }
 
 
+  //console.log(tasks)
+
   return (
-    <div className='flex flex-col justify-center items-center'>
+    <div className='flex flex-col justify-center items-center bg-slate-200 my-12 rounded-md'>
       <div className='mx-[20%] mt-6'>
       <div className='text-center text-5xl py-3 mb-5'>
-        <h1 className=''>ToDo App</h1>
+        <h1 className=''>ToDo App </h1>
        </div>
 
-       <div className='flex'>
-        <Link to={'/today'}><button id="today" onClick={handleToday} className='bg-gray-300 px-12 py-1 hover:bg-gray-400 transition ease-in-out border rounded-l-md'>Today</button></Link>
-        <Link to={'/pending'}><button id="pending" onClick={handlePending} className='bg-gray-300 px-12 py-1 hover:bg-gray-400 transition ease-in-out  border-t border-b'>Pending</button></Link>
-        <Link to={'/overdue'}><button id="overdue" onClick={handleOverDue} className='bg-gray-300 px-12 py-1 hover:bg-gray-400 transition ease-in-out  border rounded-r-md'>OverDue</button></Link>
+       <div className='flex w-1/2 md:w-full'>
+        <button id="today" onClick={handleToday} className='bg-gray-300 px-6 md:px-12 py-1 hover:bg-gray-400 transition ease-in-out border rounded-l-md'>Today</button>
+        <button id="pending" onClick={handlePending} className='bg-gray-300 px-6 md:px-12 py-1 hover:bg-gray-400 transition ease-in-out  border-t border-b'>Pending</button>
+        <button id="overdue" onClick={handleOverDue} className='bg-gray-300 px-6 md:px-12 py-1 hover:bg-gray-400 transition ease-in-out  border rounded-r-md'>OverDue</button>
        </div>
        
       </div>
        <div className='flex justify-between w-2/3 mt-12 height-96'>
-          <p className='text-3xl font-medium'>Tasks</p>
-          <button  onClick={toggleModal} className='px-9 py-1 bg-[#02521f] text-white rounded-md hover:bg-[#195e32]'>Add Task</button>
+          <p className='text-2xl md:text-3xl font-medium'>Tasks</p>
+          <button   onClick={toggleModal} className='px-4 md:px-9 py-1 flex items-end gap-2 justify-start place-content-center bg-[#02521f] text-white rounded-md hover:bg-[#195e32]'><svg className="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5"/>
+</svg>
+Add Task</button>
       </div>
-      <div className='bg-white p-4 border rounded-md w-2/3 mt-3'>
-      <div className='m-4 border border-spacing-8 bg-slate height-[1000px]'>
-
-     
-      </div>
-        <h1>This is from the task div</h1>
-        <h1>This is from the task div</h1>
-        <h1>This is from the task div</h1>
-        <h1>This is from the task div</h1>
-
-        <h1>This is from the task div</h1>
-        <h1>This is from the task div</h1>
-        <h1>This is from the task div</h1>
+      <div className='bg-white p-4 border rounded-md w-full md:w-2/3 my-3'>
+       {
+        tasks.map((task,index )=>
+          <table key={index}  className="w-full text-sm   text-gray-500 ">
+      
+          <tbody>
+           
+              <tr claclassNamess="bg-white ">
+                  <th className="px-6 py-4 font-medium w-1/3 text-left">
+                     {task.title}
+                  </th>
+                  <td className="px-6 py-4 w-1/3 text-center">
+                      {new Date(task.deadline).toLocaleDateString()}
+                  </td>
+                 
+                  <td class="px-6 py-4 w-1/3 text-center space-x-2">
+                      <button onClick={()=>handleEdit(index)}  className="font-medium  text-blue-600  hover:underline">Edit</button>
+                      <button onClick={()=>handleDelete(index)}  className="font-medium  text-blue-600  hover:underline">Delete</button>
+                  </td>
+              </tr>
+          </tbody>
+      </table>
+    
+        )
+       }
+        
 
       </div>
       <div>
@@ -93,17 +144,17 @@ function App() {
           onClick={toggleModal}
         >
           <div
-            className="relative p-4 w-full max-w-md bg-white rounded-lg shadow dark:bg-gray-700"
+            className="relative p-4 w-full max-w-md bg-white rounded-lg shadow "
             onClick={(e) => e.stopPropagation()} // Prevent click on modal to close
           >
             {/* Modal header */}
-            <div className="flex items-center justify-between p-4 border-b rounded-t dark:border-gray-600">
-              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                Create New Product
+            <div className="flex items-center justify-between p-4 border-b rounded-t ">
+              <h3 className="text-lg font-semibold text-gray-900 ">
+                Task details
               </h3>
               <button
                 onClick={toggleModal}
-                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center "
               >
                 <svg
                   className="w-3 h-3"
@@ -125,91 +176,82 @@ function App() {
             </div>
 
             {/* Modal body */}
-            <form className="p-4">
+            <form className="p-4" onSubmit={handleAddTasks}>
               <div className="grid gap-4 mb-4 grid-cols-2">
                 <div className="col-span-2">
                   <label
                     htmlFor="name"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
                   >
-                    Name
+                    Title
                   </label>
                   <input
                     type="text"
-                    name="name"
-                    id="name"
-                    placeholder="Type product name"
+                    name="title"
+                    id="title"
+                    placeholder="Add a task title"
                     required
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                   />
                 </div>
                 <div className="col-span-2 sm:col-span-1">
                   <label
                     htmlFor="price"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
                   >
-                    Price
+                    Priority
                   </label>
-                  <input
-                    type="number"
-                    name="price"
-                    id="price"
-                    placeholder="$2999"
-                    required
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  />
+                   <select
+                    id="priority"
+                    name='priority'
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  >
+                    <option  disabled className=''>Select category</option>
+                    <option value="high">High</option>
+                    <option value="medium">Medium</option>
+                    <option value="low">Low</option>
+                   
+                  </select>
                 </div>
                 <div className="col-span-2 sm:col-span-1">
                   <label
                     htmlFor="category"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
                   >
-                    Category
+                    Deadline
                   </label>
-                  <select
-                    id="category"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  >
-                    <option defaultValue>Select category</option>
-                    <option value="TV">TV/Monitors</option>
-                    <option value="PC">PC</option>
-                    <option value="GA">Gaming/Console</option>
-                    <option value="PH">Phones</option>
-                  </select>
+                 <DatePicker className="border p-2 rounded-md z-50" selected={startDate} onChange={(date) => setStartDate(date)} />
                 </div>
                 <div className="col-span-2">
                   <label
                     htmlFor="description"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
                   >
-                    Product Description
+                    Comments
                   </label>
                   <textarea
-                    id="description"
+                    id="comments"
                     rows="4"
-                    placeholder="Write product description here"
-                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    placeholder="Add your comments to your task"
+                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
                   ></textarea>
                 </div>
               </div>
-              <button
-                type="submit"
-                className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+             <div className='flex gap-2 justify-end'>
+               <button onClick={toggleModal}
+               
+                className=" inline-flex items-center text-[#02521f] border-2 border-[#02521f] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-2 text-center "
               >
-                <svg
-                  className="me-1 -ms-1 w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                Add new product
+                
+                Close
               </button>
+               <button type='submit'
+                className="text-white inline-flex items-center bg-[#02521f] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center "
+              >
+              
+                Add Task
+              </button>
+             </div>
             </form>
           </div>
         </div>
--- ToDo App/src/main.jsx
+++ ToDo App/src/main.jsx
@@ -3,33 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Today from './components/Today.jsx'
-import Pending from './components/Pending.jsx'
-import Overdue from './components/Overdue.jsx'
+
 
 const router=createBrowserRouter([
            {
             path:'/',
             element:<App></App>,
-            children:[
-              {
-                path:'/today',
-                element:<Today></Today>
-              },
-              {
-                path:'/pending',
-                element:<Pending></Pending>
-              },
-              {
-                path:'/overdue',
-                element:<Overdue></Overdue>
-              }
-            ]
            }
 ])
 
 createRoot(document.getElementById('root')).render(
- <div className='mx-[10%] bg-slate-200 mt-12 rounded-md'>
+ <div className='mx-[10%] '>
    <StrictMode>
    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
